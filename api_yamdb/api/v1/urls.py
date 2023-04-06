72d30ae5c5cb8f2e0245d4b352818d56d80999c7
Mkaniukov <109161725+Mkaniukov@users.noreply.github.com>
2023-04-06 13:37:35 +0200

    Add files via upload

---
+++ api_yamdb/api/v1/urls.py
@@ -0,0 +1,35 @@
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import (CategoryViewSet, CommentViewSet, GenreViewSet,
+                    ReviewViewSet, TitleViewSet, UserViewSet,
+                    get_jwt_token,
+                    register)
+
+router_v1 = DefaultRouter()
+router_v1.register('categories', CategoryViewSet)
+router_v1.register('genres', GenreViewSet)
+router_v1.register('titles', TitleViewSet)
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews',
+                   ReviewViewSet, basename='reviews')
+router_v1.register(r'titles/(?P<title_id>\d+)/reviews/(?P<review_id>\d+)'
+                   r'/comments', CommentViewSet, basename='comments')
+router_v1.register('users', UserViewSet)
+
+token_auth_urls = [
+    path(
+        'token/',
+        get_jwt_token,
+        name='token'
+    ),
+    path(
+        'signup/',
+        register,
+        name='register'
+    ),
+]
+
+urlpatterns = [
+    path('', include(router_v1.urls)),
+    path('auth/', include(token_auth_urls))
+]
